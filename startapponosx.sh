17ef8f7ea23e9b0fb2cc9240e88defcc43bb2d14
Hans Windhoff <hansrwindhoff@gmail.com>
2015-08-31 22:07:52 -0600

    add OS X file

---
+++ startapponosx.sh
@@ -0,0 +1,4 @@
+node-debug app.js &
+/usr/bin/open -a "/Applications/Google Chrome.app"  http://127.0.0.1:8080/?port=5858 & 
+
+/usr/bin/open -a "/Applications/Google Chrome.app"  http://localhost:81/ &
